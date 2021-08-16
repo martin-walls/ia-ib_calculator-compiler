425927e5b0714998015c428a740ab673b15f4a56
Martin Walls <44908692+martin-walls@users.noreply.github.com>
2021-08-16 17:05:35 +0100

    calculate goto

--- src/com/martinwalls/calculator_compiler/Parser.java
+++ src/com/martinwalls/calculator_compiler/Parser.java
@@ -1,5 +1,8 @@
 package com.martinwalls.calculator_compiler;
 
+import com.martinwalls.calculator_compiler.tokens.Token;
+import com.martinwalls.calculator_compiler.tokens.TokenType;
+
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
@@ -35,17 +38,47 @@ public class Parser {
     return new HashSet<>(closure);
   }
 
+  static Set<Item> allItems(Production[] productions) {
+    Set<Item> items = new HashSet<>();
+    for (Production p : productions) {
+      int length = p.body.length;
+      for (int dotPos = 0; dotPos <= length; dotPos++) {
+        items.add(new Item(p, dotPos));
+      }
+    }
+    return items;
+  }
+
+  static Set<Item> calculateGoto(Set<Item> I, Symbol X, Production[] productions) {
+    Set<Item> allItems = allItems(productions);
+    Set<Item> gotoItems = new HashSet<>();
+    for (Item j : allItems) {
+      if (j.dotPosition > 0 &&j.production.body[j.dotPosition - 1].equals(X)) {
+        for (Item i : I) {
+          if (j.production.equals(i.production) && j.dotPosition == i.dotPosition + 1) {
+            gotoItems.add(j);
+          }
+        }
+      }
+    }
+    return gotoItems;
+  }
+
   public static void main(String[] args) {
 
-    Item f = new Item(Grammar.productions[0], 0);
+    Item f = new Item(Grammar.productions[1], 1);
 
     Set<Item> i = new HashSet<>();
     i.add(f);
+//
+//    Set<Item> c = closure(i, Grammar.productions);
+//
+//    System.out.println(c);
 
-    Set<Item> c = closure(i, Grammar.productions);
+    Set<Item> g = calculateGoto(i, new Symbol(new Token(TokenType.PLUS)), Grammar.productions);
 
-    System.out.println(c);
-
+    System.out.println(g);
+    System.out.println(closure(g, Grammar.productions));
   }
 
 }
