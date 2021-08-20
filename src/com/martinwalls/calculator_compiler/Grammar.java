1facd16b36cfe2c4eb8c0592b41722130e36adee
Martin Walls <44908692+martin-walls@users.noreply.github.com>
2021-08-20 23:31:42 +0100

    refactor to Token.Type

--- src/com/martinwalls/calculator_compiler/Grammar.java
+++ src/com/martinwalls/calculator_compiler/Grammar.java
@@ -125,162 +125,162 @@ public class Grammar {
 
 
 
-  public static Map<Integer, Map<Token, Action>> actionTable = initialiseActionTable();
+  public static Map<Integer, Map<Token.Type, Action>> actionTable = initialiseActionTable();
 
-  private static Map<Integer, Map<Token, Action>> initialiseActionTable() {
-    Map<Integer, Map<Token, Action>> table = new HashMap<>();
+  private static Map<Integer, Map<Token.Type, Action>> initialiseActionTable() {
+    Map<Integer, Map<Token.Type, Action>> table = new HashMap<>();
 
-    Map<Token, Action> row0 = new HashMap<>();
-    row0.put(Token.num(), Action.shift(8));
-    row0.put(Token.cos(), Action.shift(5));
-    row0.put(Token.openBracket(), Action.shift(9));
+    Map<Token.Type, Action> row0 = new HashMap<>();
+    row0.put(Token.Type.NUM, Action.shift(8));
+    row0.put(Token.Type.COS, Action.shift(5));
+    row0.put(Token.Type.OPEN_BRACKET, Action.shift(9));
     table.put(0, row0);
 
-    Map<Token, Action> row1 = new HashMap<>();
-    row1.put(Token.plus(), Action.shift(10));
-    row1.put(Token.eol(), Action.accept());
+    Map<Token.Type, Action> row1 = new HashMap<>();
+    row1.put(Token.Type.PLUS, Action.shift(10));
+    row1.put(Token.Type.EOL, Action.accept());
     table.put(1, row1);
 
-    Map<Token, Action> row2 = new HashMap<>();
-    row2.put(Token.minus(), Action.shift(11));
-    row2.put(Token.plus(), Action.reduce(2));
-    row2.put(Token.mult(), Action.reduce(2));
-    row2.put(Token.fact(), Action.reduce(2));
-    row2.put(Token.closeBracket(), Action.reduce(2));
-    row2.put(Token.eol(), Action.reduce(2));
+    Map<Token.Type, Action> row2 = new HashMap<>();
+    row2.put(Token.Type.MINUS, Action.shift(11));
+    row2.put(Token.Type.PLUS, Action.reduce(2));
+    row2.put(Token.Type.MULT, Action.reduce(2));
+    row2.put(Token.Type.FACT, Action.reduce(2));
+    row2.put(Token.Type.CLOSE_BRACKET, Action.reduce(2));
+    row2.put(Token.Type.EOL, Action.reduce(2));
     table.put(2, row2);
 
-    Map<Token, Action> row3 = new HashMap<>();
-    row3.put(Token.minus(), Action.reduce(4));
-    row3.put(Token.plus(), Action.reduce(4));
-    row3.put(Token.mult(), Action.reduce(4));
-    row3.put(Token.fact(), Action.reduce(4));
-    row3.put(Token.closeBracket(), Action.reduce(4));
-    row3.put(Token.eol(), Action.reduce(4));
+    Map<Token.Type, Action> row3 = new HashMap<>();
+    row3.put(Token.Type.MINUS, Action.reduce(4));
+    row3.put(Token.Type.PLUS, Action.reduce(4));
+    row3.put(Token.Type.MULT, Action.reduce(4));
+    row3.put(Token.Type.FACT, Action.reduce(4));
+    row3.put(Token.Type.CLOSE_BRACKET, Action.reduce(4));
+    row3.put(Token.Type.EOL, Action.reduce(4));
     table.put(3, row3);
 
-    Map<Token, Action> row4 = new HashMap<>();
-    row4.put(Token.minus(), Action.reduce(6));
-    row4.put(Token.plus(), Action.reduce(6));
-    row4.put(Token.mult(), Action.shift(12));
-    row4.put(Token.fact(), Action.reduce(6));
-    row4.put(Token.closeBracket(), Action.reduce(6));
-    row4.put(Token.eol(), Action.reduce(6));
+    Map<Token.Type, Action> row4 = new HashMap<>();
+    row4.put(Token.Type.MINUS, Action.reduce(6));
+    row4.put(Token.Type.PLUS, Action.reduce(6));
+    row4.put(Token.Type.MULT, Action.shift(12));
+    row4.put(Token.Type.FACT, Action.reduce(6));
+    row4.put(Token.Type.CLOSE_BRACKET, Action.reduce(6));
+    row4.put(Token.Type.EOL, Action.reduce(6));
     table.put(4, row4);
 
-    Map<Token, Action> row5 = new HashMap<>();
-    row5.put(Token.num(), Action.shift(8));
-    row5.put(Token.openBracket(), Action.shift(9));
+    Map<Token.Type, Action> row5 = new HashMap<>();
+    row5.put(Token.Type.NUM, Action.shift(8));
+    row5.put(Token.Type.OPEN_BRACKET, Action.shift(9));
     table.put(5, row5);
 
-    Map<Token, Action> row6 = new HashMap<>();
-    row6.put(Token.minus(), Action.reduce(8));
-    row6.put(Token.plus(), Action.reduce(8));
-    row6.put(Token.mult(), Action.reduce(8));
-    row6.put(Token.fact(), Action.reduce(8));
-    row6.put(Token.closeBracket(), Action.reduce(8));
-    row6.put(Token.eol(), Action.reduce(8));
+    Map<Token.Type, Action> row6 = new HashMap<>();
+    row6.put(Token.Type.MINUS, Action.reduce(8));
+    row6.put(Token.Type.PLUS, Action.reduce(8));
+    row6.put(Token.Type.MULT, Action.reduce(8));
+    row6.put(Token.Type.FACT, Action.reduce(8));
+    row6.put(Token.Type.CLOSE_BRACKET, Action.reduce(8));
+    row6.put(Token.Type.EOL, Action.reduce(8));
     table.put(6, row6);
 
-    Map<Token, Action> row7 = new HashMap<>();
-    row7.put(Token.minus(), Action.reduce(10));
-    row7.put(Token.plus(), Action.reduce(10));
-    row7.put(Token.mult(), Action.reduce(10));
-    row7.put(Token.fact(), Action.shift(14));
-    row7.put(Token.closeBracket(), Action.reduce(10));
-    row7.put(Token.eol(), Action.reduce(10));
+    Map<Token.Type, Action> row7 = new HashMap<>();
+    row7.put(Token.Type.MINUS, Action.reduce(10));
+    row7.put(Token.Type.PLUS, Action.reduce(10));
+    row7.put(Token.Type.MULT, Action.reduce(10));
+    row7.put(Token.Type.FACT, Action.shift(14));
+    row7.put(Token.Type.CLOSE_BRACKET, Action.reduce(10));
+    row7.put(Token.Type.EOL, Action.reduce(10));
     table.put(7, row7);
 
-    Map<Token, Action> row8 = new HashMap<>();
-    row8.put(Token.minus(), Action.reduce(11));
-    row8.put(Token.plus(), Action.reduce(11));
-    row8.put(Token.mult(), Action.reduce(11));
-    row8.put(Token.fact(), Action.reduce(11));
-    row8.put(Token.closeBracket(), Action.reduce(11));
-    row8.put(Token.eol(), Action.reduce(11));
+    Map<Token.Type, Action> row8 = new HashMap<>();
+    row8.put(Token.Type.MINUS, Action.reduce(11));
+    row8.put(Token.Type.PLUS, Action.reduce(11));
+    row8.put(Token.Type.MULT, Action.reduce(11));
+    row8.put(Token.Type.FACT, Action.reduce(11));
+    row8.put(Token.Type.CLOSE_BRACKET, Action.reduce(11));
+    row8.put(Token.Type.EOL, Action.reduce(11));
     table.put(8, row8);
 
-    Map<Token, Action> row9 = new HashMap<>();
-    row9.put(Token.num(), Action.shift(8));
-    row9.put(Token.cos(), Action.shift(5));
-    row9.put(Token.openBracket(), Action.shift(9));
+    Map<Token.Type, Action> row9 = new HashMap<>();
+    row9.put(Token.Type.NUM, Action.shift(8));
+    row9.put(Token.Type.COS, Action.shift(5));
+    row9.put(Token.Type.OPEN_BRACKET, Action.shift(9));
     table.put(9, row9);
 
-    Map<Token, Action> row10 = new HashMap<>();
-    row10.put(Token.num(), Action.shift(8));
-    row10.put(Token.cos(), Action.shift(5));
-    row10.put(Token.openBracket(), Action.shift(9));
+    Map<Token.Type, Action> row10 = new HashMap<>();
+    row10.put(Token.Type.NUM, Action.shift(8));
+    row10.put(Token.Type.COS, Action.shift(5));
+    row10.put(Token.Type.OPEN_BRACKET, Action.shift(9));
     table.put(10, row10);
 
-    Map<Token, Action> row11 = new HashMap<>();
-    row11.put(Token.num(), Action.shift(8));
-    row11.put(Token.cos(), Action.shift(5));
-    row11.put(Token.openBracket(), Action.shift(9));
+    Map<Token.Type, Action> row11 = new HashMap<>();
+    row11.put(Token.Type.NUM, Action.shift(8));
+    row11.put(Token.Type.COS, Action.shift(5));
+    row11.put(Token.Type.OPEN_BRACKET, Action.shift(9));
     table.put(11, row11);
 
-    Map<Token, Action> row12 = new HashMap<>();
-    row12.put(Token.num(), Action.shift(8));
-    row12.put(Token.cos(), Action.shift(5));
-    row12.put(Token.openBracket(), Action.shift(9));
+    Map<Token.Type, Action> row12 = new HashMap<>();
+    row12.put(Token.Type.NUM, Action.shift(8));
+    row12.put(Token.Type.COS, Action.shift(5));
+    row12.put(Token.Type.OPEN_BRACKET, Action.shift(9));
     table.put(12, row12);
 
-    Map<Token, Action> row13 = new HashMap<>();
-    row13.put(Token.minus(), Action.reduce(7));
-    row13.put(Token.plus(), Action.reduce(7));
-    row13.put(Token.mult(), Action.reduce(7));
-    row13.put(Token.fact(), Action.reduce(7));
-    row13.put(Token.closeBracket(), Action.reduce(7));
-    row13.put(Token.eol(), Action.reduce(7));
+    Map<Token.Type, Action> row13 = new HashMap<>();
+    row13.put(Token.Type.MINUS, Action.reduce(7));
+    row13.put(Token.Type.PLUS, Action.reduce(7));
+    row13.put(Token.Type.MULT, Action.reduce(7));
+    row13.put(Token.Type.FACT, Action.reduce(7));
+    row13.put(Token.Type.CLOSE_BRACKET, Action.reduce(7));
+    row13.put(Token.Type.EOL, Action.reduce(7));
     table.put(13, row13);
 
-    Map<Token, Action> row14 = new HashMap<>();
-    row4.put(Token.minus(), Action.reduce(9));
-    row4.put(Token.plus(), Action.reduce(9));
-    row4.put(Token.mult(), Action.reduce(9));
-    row4.put(Token.fact(), Action.reduce(9));
-    row4.put(Token.closeBracket(), Action.reduce(9));
-    row4.put(Token.eol(), Action.reduce(9));
+    Map<Token.Type, Action> row14 = new HashMap<>();
+    row14.put(Token.Type.MINUS, Action.reduce(9));
+    row14.put(Token.Type.PLUS, Action.reduce(9));
+    row14.put(Token.Type.MULT, Action.reduce(9));
+    row14.put(Token.Type.FACT, Action.reduce(9));
+    row14.put(Token.Type.CLOSE_BRACKET, Action.reduce(9));
+    row14.put(Token.Type.EOL, Action.reduce(9));
     table.put(14, row14);
 
-    Map<Token, Action> row15 = new HashMap<>();
-    row15.put(Token.plus(), Action.shift(10));
-    row15.put(Token.closeBracket(), Action.shift(19));
+    Map<Token.Type, Action> row15 = new HashMap<>();
+    row15.put(Token.Type.PLUS, Action.shift(10));
+    row15.put(Token.Type.CLOSE_BRACKET, Action.shift(19));
     table.put(15, row15);
 
-    Map<Token, Action> row16 = new HashMap<>();
-    row16.put(Token.minus(), Action.shift(11));
-    row16.put(Token.plus(), Action.reduce(1));
-    row16.put(Token.mult(), Action.reduce(1));
-    row16.put(Token.fact(), Action.reduce(1));
-    row16.put(Token.closeBracket(), Action.reduce(1));
-    row16.put(Token.eol(), Action.reduce(1));
+    Map<Token.Type, Action> row16 = new HashMap<>();
+    row16.put(Token.Type.MINUS, Action.shift(11));
+    row16.put(Token.Type.PLUS, Action.reduce(1));
+    row16.put(Token.Type.MULT, Action.reduce(1));
+    row16.put(Token.Type.FACT, Action.reduce(1));
+    row16.put(Token.Type.CLOSE_BRACKET, Action.reduce(1));
+    row16.put(Token.Type.EOL, Action.reduce(1));
     table.put(16, row16);
 
-    Map<Token, Action> row17 = new HashMap<>();
-    row17.put(Token.minus(), Action.reduce(3));
-    row17.put(Token.plus(), Action.reduce(3));
-    row17.put(Token.mult(), Action.reduce(3));
-    row17.put(Token.fact(), Action.reduce(3));
-    row17.put(Token.closeBracket(), Action.reduce(3));
-    row17.put(Token.eol(), Action.reduce(3));
+    Map<Token.Type, Action> row17 = new HashMap<>();
+    row17.put(Token.Type.MINUS, Action.reduce(3));
+    row17.put(Token.Type.PLUS, Action.reduce(3));
+    row17.put(Token.Type.MULT, Action.reduce(3));
+    row17.put(Token.Type.FACT, Action.reduce(3));
+    row17.put(Token.Type.CLOSE_BRACKET, Action.reduce(3));
+    row17.put(Token.Type.EOL, Action.reduce(3));
     table.put(17, row17);
 
-    Map<Token, Action> row18 = new HashMap<>();
-    row18.put(Token.minus(), Action.reduce(5));
-    row18.put(Token.plus(), Action.reduce(5));
-    row18.put(Token.mult(), Action.reduce(5));
-    row18.put(Token.fact(), Action.reduce(5));
-    row18.put(Token.closeBracket(), Action.reduce(5));
-    row18.put(Token.eol(), Action.reduce(5));
+    Map<Token.Type, Action> row18 = new HashMap<>();
+    row18.put(Token.Type.MINUS, Action.reduce(5));
+    row18.put(Token.Type.PLUS, Action.reduce(5));
+    row18.put(Token.Type.MULT, Action.reduce(5));
+    row18.put(Token.Type.FACT, Action.reduce(5));
+    row18.put(Token.Type.CLOSE_BRACKET, Action.reduce(5));
+    row18.put(Token.Type.EOL, Action.reduce(5));
     table.put(18, row18);
 
-    Map<Token, Action> row19 = new HashMap<>();
-    row19.put(Token.minus(), Action.reduce(12));
-    row19.put(Token.plus(), Action.reduce(12));
-    row19.put(Token.mult(), Action.reduce(12));
-    row19.put(Token.fact(), Action.reduce(12));
-    row19.put(Token.closeBracket(), Action.reduce(12));
-    row19.put(Token.eol(), Action.reduce(12));
+    Map<Token.Type, Action> row19 = new HashMap<>();
+    row19.put(Token.Type.MINUS, Action.reduce(12));
+    row19.put(Token.Type.PLUS, Action.reduce(12));
+    row19.put(Token.Type.MULT, Action.reduce(12));
+    row19.put(Token.Type.FACT, Action.reduce(12));
+    row19.put(Token.Type.CLOSE_BRACKET, Action.reduce(12));
+    row19.put(Token.Type.EOL, Action.reduce(12));
     table.put(19, row19);
 
     return table;
