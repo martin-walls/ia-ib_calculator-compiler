2427f716e27487bdda366f5d3191bc1a365b145a
Martin Walls <44908692+martin-walls@users.noreply.github.com>
2021-08-21 14:05:19 +0100

    print tree

--- src/com/martinwalls/calculator_compiler/ParseTree.java
+++ src/com/martinwalls/calculator_compiler/ParseTree.java
@@ -2,16 +2,25 @@ package com.martinwalls.calculator_compiler;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 
 public class ParseTree {
 
-  Symbol root;
-  List<ParseTree> children;
+  final Symbol root;
+  final List<ParseTree> children;
 
   public ParseTree(Symbol root, List<ParseTree> children) {
     this.root = root;
     this.children = children;
+  }
+
+  public Symbol getRoot() {
+    return root;
+  }
+
+  public List<ParseTree> getChildren() {
+    return children;
   }
 
   public ParseTree(Symbol root) {
@@ -26,6 +35,49 @@ public class ParseTree {
 
   @Override
   public String toString() {
-    return root + (children.isEmpty() ? "" : " " + children);
+    StringBuilder sb = new StringBuilder();
+    printTreeIntoBuffer(sb, "", "");
+    return sb.toString();
   }
+
+  /**
+   * Build up string representation of the parse tree
+   * @param buffer a string buffer to build the tree representation into
+   * @param rootPrefix print this before (to the left of) the root of the tree; so we can
+   *                   display nesting correctly
+   * @param childrenPrefix print this before (to the left of) any subsequent children; so
+   *                       we can display nesting correctly
+   */
+  private void printTreeIntoBuffer(StringBuilder buffer, String rootPrefix, String childrenPrefix) {
+    buffer.append(rootPrefix);
+    buffer.append(root);
+    int rootNameLen = root.toString().length();
+
+    if (children.size() == 1) {
+      // if only one child, print it on same line
+      children.get(0).printTreeIntoBuffer(buffer,
+          " ─ ",
+          childrenPrefix + "   "  + " ".repeat(rootNameLen));
+    } else {
+      // otherwise, with more or zero children, new line
+      buffer.append("\n");
+
+      for (Iterator<ParseTree> it = children.iterator(); it.hasNext(); ) {
+        ParseTree nextChild = it.next();
+        if (it.hasNext()) {
+          nextChild.printTreeIntoBuffer(buffer,
+              childrenPrefix + "├" + "─".repeat(rootNameLen) + " ",
+              childrenPrefix + "| " + " ".repeat(rootNameLen));
+        } else {
+          // last child
+          nextChild.printTreeIntoBuffer(buffer,
+              childrenPrefix + "└" + "─".repeat(rootNameLen) + " ",
+              childrenPrefix + "  " + " ".repeat(rootNameLen));
+        }
+      }
+    }
+  }
+
+
+
 }
